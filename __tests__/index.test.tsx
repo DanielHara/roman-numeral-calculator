b34a4e40b5e683d3e4081865a2afa4e208661076
Daniel Hara <haradaniel734@gmail.com>
2022-09-18 19:55:16 +0200

    case 10 -> X and 11 -> XI

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -28,6 +28,8 @@ describe('when user enters', () => {
     [7, 'VII'],
     [8, 'VIII'],
     [9, 'IX'],
+    [10, 'X'],
+    [11, 'XI'],
   ])('%s, then %s is displayed', (integer, roman) => {
     render(<Calculator />);
 
