ab6e6f83a85440a0661901c4f6084fdbb457ac47
Daniel Hara <haradaniel734@gmail.com>
2022-09-18 19:41:05 +0200

    make first interaction tests pass

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,27 @@
+import { useState } from 'react';
+
+import intToRoman from "./intToRoman";
+
 export default function Home() {
+  const [number, setNumber] = useState(1);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNumber(Number(e.target.value));
+  };
+
   return (
     <div>
       <div>
         <label htmlFor="number">
           Enter number here:
         </label>
-        <input id="number" name="number"></input>
+        <input id="number" name="number" type="number" onChange={handleChange}></input>
       </div>
       <div>
         <label htmlFor="roman">
           Roman number:
         </label>
-        <input id="roman" name="roman"></input>
+        <input id="roman" name="roman" value={intToRoman(number)} readOnly></input>
       </div>
     </div>
   )
