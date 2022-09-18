9f9908181652bc479db5f380590f0fa7e3aa5b63
Daniel Hara <haradaniel734@gmail.com>
2022-09-18 19:39:58 +0200

    test input change

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Calculator from '@/pages/index';
 
 describe('rendering', () => {
@@ -14,5 +14,29 @@ describe('rendering', () => {
     const output = screen.getByLabelText('Roman number:');
 
     expect(output).toBeInTheDocument();
+  });
+});
+
+describe('when user enters', () => {
+  it('1, then "I" is displayed', () => {
+    render(<Calculator />);
+
+    const input = screen.getByLabelText('Enter number here:');
+
+    fireEvent.change(input, { target: { value: 1 } });
+
+    const output = screen.getByLabelText('Roman number:');
+    expect(output).toHaveValue('I')
+  });
+
+  it('3, then "III" is displayed', () => {
+    render(<Calculator />);
+
+    const input = screen.getByLabelText('Enter number here:');
+
+    fireEvent.change(input, { target: { value: 3 } });
+
+    const output = screen.getByLabelText('Roman number:');
+    expect(output).toHaveValue('III')
   });
 });
